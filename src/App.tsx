772df7c7a09195203bcdaca9677979bd850ab661
Connie Too <115683397+c2-code@users.noreply.github.com>
2024-09-24 01:37:36 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Labs from './Labs';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to Web Dev</h1>
+     <Labs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Labs/Lab6/index.tsx
@@ -0,0 +1,7 @@
+export default function Lab6() {
+    return (
+        <div> 
+            <h2>Lab6</h2>
+        </div>
+    )
+}
