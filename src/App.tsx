a809a98c0d0638a10500c8ecae82c991794b0b54
Connie Too <115683397+c2-code@users.noreply.github.com>
2024-09-24 22:07:23 -0400

    a1 submission final

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom"; 
-import Kanbas from "./Kanbas";
+import React from 'react';
 import Labs from "./Labs";
-export default function App() {
+import Kanbas from "./Kanbas";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom"; 
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
   return (
     <HashRouter>
-    <div>
-      <Routes>
-      <Route path="/" element={<Navigate to="Kanbas" />} />
-      <Route path="/Labs/*" element={<Labs />} />
-      <Route path="/Kanbas/*" element={<Kanbas />} />
-      </Routes>
-    </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
     </HashRouter>
   );
 }
+export default App;
